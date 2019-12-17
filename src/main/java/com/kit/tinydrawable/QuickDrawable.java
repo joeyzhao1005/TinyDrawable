814f481d96fb08ac942ae74f9ccc6b876f46ef75
joeyzhao <joeyzhao1005@gmail.com>
2019-12-17 22:07:32 +0800

    [fix] tinydrawable

---
+++ src/main/java/com/kit/tinydrawable/QuickDrawable.java
@@ -0,0 +1,257 @@
+package com.kit.tinydrawable;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kit.utils.DensityUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.graphics.drawable.GradientDrawable.RECTANGLE;
+
+public class QuickDrawable {
+
+
+    //默认矩形
+    private int shape = RECTANGLE;
+
+    //边框宽度，默认0
+    private int borderWidth = 0;
+    //边框颜色
+    private int borderColor = Color.GRAY;
+    //背景颜色
+    private int backgroundColor = Color.WHITE;
+
+
+    //圆角半径
+    private float topRightRadius = 5;
+    private float topLeftRadius = 5;
+    private float bottomRightRadius = 5;
+    private float bottomLeftRadius = 5;
+
+
+    //虚线宽度
+    private int dashWidth = 0;
+
+    //虚线间隔
+    private float dashGap = 0;
+
+
+    //存放stateSet
+    Map<Integer, Drawable> stateMap = new HashMap<>();
+
+    ColorStateList colorStateList;
+
+
+    public QuickDrawable() {
+
+
+    }
+
+    //初始化基础属性
+    private void iniParms() {
+        borderWidth = 0;
+        borderColor = Color.GRAY;
+        backgroundColor = Color.WHITE;
+        topRightRadius = 5;
+        topLeftRadius = 5;
+        bottomRightRadius = 5;
+        bottomLeftRadius = 5;
+    }
+
+    private void resetParms() {
+        iniParms();
+        colorStateList = null;
+        stateMap.clear();
+    }
+
+    private Drawable createDrawable() {
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setShape(shape);
+        drawable.setStroke(borderWidth, borderColor, dashWidth, dashGap);
+        drawable.setColor(backgroundColor);
+        drawable.setCornerRadii(new float[]{topLeftRadius, topLeftRadius, topRightRadius, topRightRadius, bottomRightRadius, bottomRightRadius, bottomLeftRadius, bottomLeftRadius});
+        return drawable;
+    }
+
+
+    private Drawable build() {
+        if (stateMap.size() > 0) {
+            StateListDrawable stateListDrawable = new StateListDrawable();
+            for (Map.Entry<Integer, Drawable> entry : stateMap.entrySet()) {
+                stateListDrawable.addState(new int[]{entry.getKey()}, entry.getValue());
+            }
+            return stateListDrawable;
+        } else {
+            return createDrawable();
+        }
+    }
+
+
+    public QuickDrawable border() {
+        borderWidth = DensityUtils.dip2px(1);
+        return this;
+    }
+
+    public QuickDrawable border(int width) {
+        this.borderWidth = DensityUtils.dip2px(width);
+        return this;
+    }
+
+    public QuickDrawable borderColor(int color) {
+        if (borderWidth == 0 && dashWidth == 0) {
+            borderWidth = 1;
+        }
+        this.borderColor = color;
+        return this;
+    }
+
+    public QuickDrawable dash(int width) {
+        this.dashWidth = width;
+        return this;
+    }
+
+    public QuickDrawable dashGap(int gap) {
+        this.dashGap = gap;
+        return this;
+    }
+
+
+    public QuickDrawable corner() {
+
+        return this;
+    }
+
+    public QuickDrawable corner(int radius) {
+        corner(radius, radius, radius, radius);
+        return this;
+    }
+
+    public QuickDrawable corner(int topLeft, int topRight, int bottomLeft, int bottomRight) {
+        this.topLeftRadius = DensityUtils.dip2px(topLeft);
+        this.topRightRadius = DensityUtils.dip2px(topRight);
+        this.bottomLeftRadius = DensityUtils.dip2px(bottomLeft);
+        this.bottomRightRadius = DensityUtils.dip2px(bottomRight);
+        return this;
+    }
+
+    public QuickDrawable color(int color) {
+        this.backgroundColor = color;
+        return this;
+    }
+
+    public QuickDrawable setPressed() {
+        return this;
+    }
+
+    /**
+     * 设置state_pressed的效果
+     *
+     * @param backgroundColor 背景颜色
+     * @param b               对应state_xxx 的布尔值属性
+     * @return
+     */
+    public QuickDrawable addPressed(int backgroundColor, boolean b) {
+        putStateColor(b ? android.R.attr.state_pressed : -android.R.attr.state_pressed, backgroundColor);
+        return this;
+    }
+
+    public QuickDrawable addPressedDrawable(Drawable drawable, boolean b) {
+        stateMap.put(b ? android.R.attr.state_pressed : -android.R.attr.state_pressed, drawable);
+        return this;
+    }
+
+
+    /**
+     * 将之前的操作添加到state_pressed的drawable
+     *
+     * @param b
+     * @return
+     */
+    public QuickDrawable addToPressed(boolean b) {
+        Drawable drawable = createDrawable();
+        iniParms();
+        stateMap.put(b ? android.R.attr.state_pressed : -android.R.attr.state_pressed, drawable);
+        return this;
+    }
+
+
+    /**
+     * @param backgroundColor 背景颜色
+     * @param b               对应state_xxx 的布尔值属性
+     * @return
+     */
+    public QuickDrawable addEnable(int backgroundColor, boolean b) {
+        putStateColor(b ? android.R.attr.state_enabled : -android.R.attr.state_enabled, backgroundColor);
+        return this;
+    }
+
+    public QuickDrawable addEnableDrawable(Drawable drawable, boolean b) {
+        stateMap.put(b ? android.R.attr.state_enabled : -android.R.attr.state_enabled, drawable);
+        return this;
+    }
+
+    public QuickDrawable addToEnable(boolean b) {
+        Drawable drawable = createDrawable();
+        iniParms();
+        stateMap.put(b ? android.R.attr.state_enabled : -android.R.attr.state_enabled, drawable);
+        return this;
+    }
+
+
+    /**
+     * @param backgroundColor 背景颜色
+     * @param b               对应state_xxx 的布尔值属性
+     * @return
+     */
+    public QuickDrawable addChecked(int backgroundColor, boolean b) {
+        putStateColor(b ? android.R.attr.state_checked : -android.R.attr.state_checked, backgroundColor);
+        return this;
+    }
+
+    public QuickDrawable addCheckedDrawable(Drawable drawable, boolean b) {
+        stateMap.put(b ? android.R.attr.state_checked : -android.R.attr.state_checked, drawable);
+        return this;
+    }
+
+
+    public QuickDrawable addToChecked(boolean b) {
+        Drawable drawable = createDrawable();
+        iniParms();
+        stateMap.put(b ? android.R.attr.state_checked : -android.R.attr.state_checked, drawable);
+        return this;
+    }
+
+
+    private void putStateColor(int state, int color) {
+        ColorDrawable colorDrawable = new ColorDrawable();
+        colorDrawable.setColor(color);
+        stateMap.put(state, colorDrawable);
+    }
+
+
+    public QuickDrawable textColor(int uncheck, int check) {
+        // 颜色数组
+        int[] colors = new int[]{check, check, uncheck, uncheck, uncheck, uncheck, uncheck};
+
+// 颜色数组对应的状态
+        int[][] states = new int[7][];
+        states[0] = new int[]{android.R.attr.state_checked, android.R.attr.state_enabled};
+        states[1] = new int[]{android.R.attr.state_pressed, android.R.attr.state_enabled};
+        states[2] = new int[]{android.R.attr.state_enabled, android.R.attr.state_focused};
+        states[3] = new int[]{android.R.attr.state_enabled};
+        states[4] = new int[]{android.R.attr.state_focused};
+        states[5] = new int[]{android.R.attr.state_window_focused};
+        states[6] = new int[]{};
+        colorStateList = new ColorStateList(states, colors);
+        return this;
+    }
+
+}
---
+++ src/main/java/com/kit/tinydrawable/TinyDrawable.java
@@ -0,0 +1,169 @@
+package com.kit.tinydrawable;
+
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.util.LruCache;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.IntDef;
+import androidx.annotation.Nullable;
+
+import com.kit.utils.ApiLevel;
+import com.kit.utils.ValueOf;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static android.graphics.drawable.GradientDrawable.LINE;
+import static android.graphics.drawable.GradientDrawable.OVAL;
+import static android.graphics.drawable.GradientDrawable.RECTANGLE;
+import static android.graphics.drawable.GradientDrawable.RING;
+
+@SuppressWarnings("AlibabaAvoidDoubleOrFloatEqualCompare")
+public class TinyDrawable {
+    private static LruCache<String, Drawable> drawableLruCache;
+
+    public static void init(int size) {
+        if (size > 0) {
+            drawableLruCache = new LruCache<>(size);
+        } else {
+            drawableLruCache = new LruCache<>(30);
+        }
+    }
+
+    public TinyDrawable setup() {
+        return new TinyDrawable();
+    }
+
+
+    public Drawable create() {
+        String solidStr = "";
+        if (colorStateList != null) {
+            solidStr = colorStateList.toString();
+        } else {
+            solidStr = ValueOf.toString(solid);
+        }
+
+        StringBuilder radiiStr = new StringBuilder();
+        if (radii != null) {
+            boolean allEquals = true;
+            float temp = radii[0];
+            for (float f : radii) {
+                allEquals = allEquals && (temp - f < 0.01);
+            }
+            if (allEquals) {
+                radiiStr = new StringBuilder(ValueOf.toString(radii[0]));
+            } else {
+                for (float f : radii) {
+                    radiiStr.append(ValueOf.toString(f));
+                }
+            }
+
+        } else {
+            solidStr = ValueOf.toString(radius);
+        }
+
+
+        String key = shape + solidStr + stroke + strokeColor + radiiStr;
+
+        Drawable saved = drawableLruCache.get(key);
+        if (saved != null) {
+            return saved;
+        } else {
+            GradientDrawable drawable = new GradientDrawable();
+            //设置圆环宽高
+            drawable.setSize(width, height);
+
+            //设置形状为圆环
+            drawable.setShape(shape);
+
+            //设置圆环内部填充颜色
+            if (colorStateList != null && ApiLevel.ATLEAST_LOLLIPOP) {
+                drawable.setColor(colorStateList);
+            } else {
+                drawable.setColor(solid);
+            }
+
+            //设置圆环宽度以及颜色
+            if (stroke > 0) {
+                drawable.setStroke(stroke, strokeColor);
+            }
+            if (radii != null) {
+                drawable.setCornerRadii(radii);
+            } else if (radius > 0) {
+                drawable.setCornerRadius(radius);
+            }
+            //设置圆环中心点位置
+//        drawable.setGradientCenter(100, 200);
+
+            drawableLruCache.put(key, drawable);
+            return drawable;
+        }
+    }
+
+
+    public TinyDrawable width(int width) {
+        this.width = width;
+        return this;
+    }
+
+    public TinyDrawable height(int height) {
+        this.height = height;
+        return this;
+    }
+
+    public TinyDrawable setCornerRadii(@Nullable float[] radii) {
+        this.radii = radii;
+        return this;
+    }
+
+
+    public TinyDrawable setCornerRadius(float radius) {
+        this.radius = radius;
+        return this;
+    }
+
+    public TinyDrawable strokeColor(@ColorInt int strokeColor) {
+        this.strokeColor = strokeColor;
+        return this;
+    }
+
+    public TinyDrawable stroke(int stroke) {
+        this.stroke = stroke;
+        return this;
+    }
+
+    public TinyDrawable colorStateList(ColorStateList colorStateList) {
+        this.colorStateList = colorStateList;
+        return this;
+    }
+
+    public TinyDrawable solid(@ColorInt int solid) {
+        this.solid = solid;
+        return this;
+    }
+
+    public TinyDrawable shape(@Shape int shape) {
+        this.shape = shape;
+        return this;
+    }
+
+
+    private int shape;
+    private int solid;
+    private int stroke;
+    private int strokeColor;
+    private int width, height;
+    private ColorStateList colorStateList;
+
+    private float[] radii;
+    private float radius;
+
+
+    @IntDef({RECTANGLE, OVAL, LINE, RING})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Shape {
+    }
+
+}
